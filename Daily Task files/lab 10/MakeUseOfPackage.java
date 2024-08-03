6ff8d67cd65977c984e9768e336800b4ae11a574
Karthik4848 <111231442+Karthik4322@users.noreply.github.com>
2024-08-03 22:09:51 +0530

    Add files via upload

---
+++ Daily Task files/lab 10/MakeUseOfPackage.java
@@ -0,0 +1,19 @@
+import arithmeticoperations.*;
+import java.io.*;
+
+public MakeUseOfPackage{
+public static void main(String args[]) throws IOException{
+
+Addition a1 = new Addition();
+Subtraction s1 = new Subtraction();
+Multiplication m1 = new Multiplication();
+Division d1 = new Division();
+
+a1.sum();
+s1.difference();
+m1.multiply();
+d1.divide();
+
+}
+}
+
