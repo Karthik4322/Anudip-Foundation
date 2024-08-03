a930c9dc6f9f31febe1048bb07bc95573c0d3be0
Karthik4848 <111231442+Karthik4322@users.noreply.github.com>
2024-08-03 21:18:16 +0530

    Add files via upload

---
+++ Daily Task files/lab 14/MultipleExceptions.java
@@ -0,0 +1,26 @@
+public class MultipleExceptions {
+
+    public static void getExceptions() throws ArithmeticException, ArrayIndexOutOfBoundsException {
+        int a = 20;
+        int b = 0;
+        int arr[] = new int[2];
+
+        // This will cause ArithmeticException
+        b = a / b;
+
+        // This will cause ArrayIndexOutOfBoundsException
+        System.out.println("The value at index 3 is :" + arr[2]);
+    }
+    public static void main(String args[]) {
+        try {
+            getExceptions(); // In try section is used to have exception statements
+        } 
+        catch (ArithmeticException e) { // In catch section used to return statement if valid arithmeticexception received from the try block
+            System.out.println("An arithmetic problem occurred: " + e.getMessage());
+        } catch (ArrayIndexOutOfBoundsException ae) { //In catch section used to return statement if valid arrayindexoutofboundsexception received from the try block
+
+            System.out.println("An array index out of bounds problem occurred: " + ae.getMessage());
+        }
+        System.out.println("The program works fine");
+    }
+}
---
+++ Daily Task files/lab 14/ThrowExample.java
@@ -0,0 +1,13 @@
+class ThrowExample{
+    int age;
+   static void validateAge(int age){ // static method to call method without objectname
+        if(age<18)
+        throw new ArithmeticException("Not valid"); // if throw exception is happened , it doesn't allow remaining program to execute
+        else
+        System.out.println("He/she is allowed to vote");
+    }
+    public static void main(String[] args) {
+        validateAge(22);  // passing the value to parameter int age in method named validateAge
+        System.out.println("command reader");
+    }
+}
